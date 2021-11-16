8e94df8d9830ff8e75e81a12cf10557c6edc0f6b
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-11-16 14:10:24 +0100

    iteration end

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Celebrity = require("../models/Celebrity.model");
 const Movie = require("../models/Movie.model")
 
-
+//MOVIE CREATION
 
 router
     .route("/create")
@@ -28,14 +28,56 @@ router
 
     })
 
-    router.get("/delete/:id",(req, res)=>{
-        Movie.findByIdAndDelete(req.params.id)
-        .then(() => res.redirect("/movies"))
+    //MOVIE EDITION
+    router
+    .route("/:id/edit")
+    .get(async (req, res)=>{
+        try{
+            const allCelebs = await Celebrity.find()
+            const movie = await Movie.findById(req.params.id)
+            res.render("movies/edit-movie.hbs", {allCelebs, movie: movie})
+
+        }catch(error){
+            console.log(error)
+        }
+    })
+    .post(async (req,res)=>{
+        try{
+        const movieId = req.params.id
+        const {title, genre, plot, cast} = req.body
+        const foundMovie = await Movie.findByIdAndUpdate(movieId, {title, genre, plot, cast})
+        .populate("cast")
+        res.redirect("/movies")
+        }catch(error){
+            console.log(error)
+        }
+
+    })
+
+
+    //MOVIE DELEETE
+
+    router.post("/:id/delete", (req,res)=>{
+        console.log("hola")
+        const movieId= req.params.id
+        Movie.findByIdAndDelete(movieId)
+        .then(deletedMovie => res.redirect("/movies"))
         .catch(error=> console.log(error))
-      })
+      
+    })
+
+    //MOVIE DETAILS
+    router.get("/:id", (req,res)=>{
+        const movieId = req.params.id
+        Movie.findById(movieId).populate("cast")
+        .then((movies)=>{
+            res.render("movies/movie-details", movies)
+        })
+    })
 
     router.get("/", (req,res)=>{
-        Movie.find().populate("cast")
+        Movie.find()
+        .populate("cast")
         .then((movies)=>{
             res.render("movies/movies", {movies})
         })
